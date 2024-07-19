525fa6fc70e81b32e56f70883a2b1e9292039fea
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2024-07-19 18:06:12 +0100

    Add files via upload
    SagsMan develop a grade calculator using python code

---
+++ Grade calculator using python.py
@@ -0,0 +1,21 @@
+print("    "       "{GRADE CALCULATOR}"           "    ")
+
+A = input(input("Dear STUDENT your Name: "))
+score = int(input("Enter your Score: "))
+if 70 <= score <= 100:
+  print("EXCELLENT!!!")
+elif 60<= score<= 69:
+	print("VERY GOOD!!")
+elif 50<= score<= 59:
+	print("GOOD!")
+elif 45<= score<= 49:
+	print("FAIRLY GOOD")
+elif 40<= score<= 44:
+	print("PASS")
+else: 
+	print("FAIL") 
+	f
+
+
+
+#this was CREATED BY ESTHER OGAH
